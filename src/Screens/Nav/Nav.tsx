0356b49585afb20a181fb23ac9dc04dc0f4cc6d0
Utkarsh Verma <108287933+Utkarshvr@users.noreply.github.com>
2024-03-23 18:07:43 +0530

    nav z-index set to 10

--- src/Screens/Nav/Nav.tsx
+++ src/Screens/Nav/Nav.tsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 export default function Nav() {
   return (
-    <header className="min-h-[8vh] p-4 bg-slate-950 fixed w-full top-0 left-0 flex items-center justify-center">
+    <header className="min-h-[8vh] z-10 p-4 bg-slate-950 fixed w-full top-0 left-0 flex items-center justify-center">
       {/* <img src="/logo.png" className="w-12 h-12 rounded-full" /> */}
 
       <div className="flex items-center gap-3">
         {["About", "Work", "Contact"].map((link) => (
           <Link
             href={`#${link.toLowerCase()}`}
-            className="text-sm font-medium text-netrual-200 hover:font-bold transition-all"
+            className="text-sm font-bold text-netrual-200 hover:font-medium transition-all"
           >
             {link}
           </Link>
